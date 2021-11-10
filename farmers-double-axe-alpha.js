d19ef1715b98b2164ada95fcd38a52db6f16bb38
ZRADNYK <wordy7485@gmail.com>
2021-11-10 17:03:15 +0200

    Update farmers-double-axe-alpha.js

--- farmers-double-axe-alpha.js
+++ farmers-double-axe-alpha.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Farmers World Bot
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Let's farm easy way
 // @author       ZRADNYK
 // @match        https://play.farmersworld.io
