583ceba73d242b8a48470755df5e2ade48b3888e
ZRADNYK <wordy7485@gmail.com>
2021-11-10 16:42:36 +0200

    Update farmers.js
    fixed cd issue

--- acer/farmers.js
+++ acer/farmers.js
@@ -4,52 +4,61 @@ let goldIcon = document.querySelector("#root > div > div > div > section.contain
 let mineButton = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.home-card-button__group > div:nth-child(1) > button > div")
 let timeSelector = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.info-time > div");
 const delay = ms => new Promise(res => setTimeout(res, ms));
-let timeLeft;
+let timeLeft, id;
 
 async function start() {
-	timeLeft =  await checkTime();
-	console.log('time left: ', timeLeft);
-	if(timeLeft == '00:00:00') {
-		await mine(firstItem);
-		await mine(secondItem);
-		console.log('mined at ' + new Date());
-		let nextMineAt = new Date();
-		nextMineAt.setHours(nextMineAt.getHours() + 1);
-		console.log('Next mine at ' + nextMineAt); 	
-		setTimeout(start, 60 * 61 * 1000);
-	}
-	else {
-		console.log('waiting for ', timeLeft);
-		let timeArray = timeLeft.split(':');
-		let mils = (timeArray[0] * 60 * 60 + timeArray[1] * 60 + timeArray[2]) * 1000;
-		setTimeout(start, mils);
-	}
-	
+    timeLeft =  await getCooldown();
+    let timeLeftMillis = stringToTime(timeLeft);
+    console.log(new Date().toString() + ' Current cooldown : ' + timeLeft);
+    if(timeLeftMillis === 0) {
+        await mine(firstItem);
+        await mine(secondItem);
+        console.log('mine!');
+        console.log('mined at ' + new Date());
+        let cd = await getCooldown();
+        let cdInMillis = stringToTime(cd);
+        let nextMineAt = new Date(Date.now() + cdInMillis);
+        console.log('Next mine at ' + nextMineAt);
+        id = setTimeout(start, cdInMillis);
+    }
+    else {
+        console.log('waiting for ', timeLeft);
+
+        id = setTimeout(start, timeLeftMillis);
+    }
+
 }
 
 async function mine(item) {
-	if(item !== undefined) {
-		item.click();
-		await delay(3000);
-		
-		mineButton.click();
-		await delay(7000);
-		
-		goldIcon.click();
-		await delay(2000);
-	}
+    if(item !== undefined) {
+        item.click();
+await delay(3000);
+
+        mineButton.click();
+        await delay(7000);
+
+        goldIcon.click();
+        await delay(2000);
+    }
 }
 
-async function checkTime() {
-	firstItem.click();
-	await delay(1000);
-	let firstItemTimeLeft = timeSelector.innerText;
-	secondItem.click();
-	await delay(1000);
-	let secondItemTimeLeft = timeSelector.innerText;
-	firstItemTimeLeft > secondItemTimeLeft ? timeLeft = firstItemTimeLeft : timeLeft = secondItemTimeLeft;
-	return timeLeft;
+async function getCooldown() {
+    firstItem.click();
+    await delay(1000);
+    let firstItemTimeLeft = timeSelector.innerText;
+    secondItem.click();
+    await delay(1000);
+    let secondItemTimeLeft = timeSelector.innerText;
+    firstItemTimeLeft > secondItemTimeLeft ? timeLeft = firstItemTimeLeft : timeLeft = secondItemTimeLeft;
+    return timeLeft;
+}
+
+function stringToTime(str) {
+    let timeArray = str.split(':');
+  console.log(timeArray);
+  let millis = (Number.parseInt(timeArray[0]) * 60 * 60 + Number.parseInt(timeArray[1]) * 60 + Number.parseInt(timeArray[2])) * 1000;
+  console.log('millis: ', millis);
+    return millis;
 }
 
 start();
-
