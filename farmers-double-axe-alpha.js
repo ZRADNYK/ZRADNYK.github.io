4fd4edba2ea4b824f31403f467d3048c9195818c
ZRADNYK <wordy7485@gmail.com>
2021-11-10 16:58:15 +0200

    Update farmers-double-axe-alpha.js
    added login functionality

--- farmers-double-axe-alpha.js
+++ farmers-double-axe-alpha.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Farmers World Bots
+// @name         Farmers World Bot
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.1.1
+// @description  Let's farm easy way
+// @author       ZRADNYK
 // @match        https://play.farmersworld.io
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -12,11 +12,16 @@
 (function() {
     'use strict';
 
-let firstItem = document.querySelector("#root > div > div > div > div.wapper > section > div > section > img");
-let secondItem = document.querySelector("#root > div > div > div > div.wapper > section > div > section > img:nth-child(2)");
-let goldIcon = document.querySelector("#root > div > div > div > section.container__header > div:nth-child(1) > i > img");
-let mineButton = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.home-card-button__group > div:nth-child(1) > button > div")
-let timeSelector = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.info-time > div");
+let firstItem = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.card-section > div.card-img > img")
+const firstItem = document.querySelector("#root > div > div > div > div.wapper > section > div > section > img");
+const secondItem = document.querySelector("#root > div > div > div > div.wapper > section > div > section > img:nth-child(2)");
+const goldIcon = document.querySelector("#root > div > div > div > section.container__header > div:nth-child(1) > i > img");
+const mineButton = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.home-card-button__group > div:nth-child(1) > button > div")
+const timeSelector = document.querySelector("#root > div > div > div > div.wapper > section > div > div > div.info-section > div.info-time > div");
+const loginButton = document.querySelector("#root > div > div > div > button");
+const waxWalletAccount = document.querySelector("#root > div > div > div:nth-child(2) > div.login-modal-container > button:nth-child(2) > p");
+
+
 const delay = ms => new Promise(res => setTimeout(res, ms));
 let timeLeft, id;
 
@@ -69,12 +74,25 @@ async function getCooldown() {
 
 function stringToTime(str) {
     let timeArray = str.split(':');
-  console.log(timeArray);
-  let millis = (Number.parseInt(timeArray[0]) * 60 * 60 + Number.parseInt(timeArray[1]) * 60 + Number.parseInt(timeArray[2])) * 1000;
-  console.log('millis: ', millis);
+	console.log(timeArray);
+	let millis = (Number.parseInt(timeArray[0]) * 60 * 60 + Number.parseInt(timeArray[1]) * 60 + Number.parseInt(timeArray[2])) * 1000;
+	console.log('millis: ', millis);
     return millis;
 }
 
+async function checkAuthorize() {
+	if (loginButton !== undefined) {
+		loginButton.click();
+		await delay(2000);
+	}
+	if(waxWalletAccount !== undefined) {
+		waxWalletAccount.click();
+		await delay(10000);
+	}
+}
+
+await checkAuthorize();
 start();
-// Your code here...
+    
+    
 })();
