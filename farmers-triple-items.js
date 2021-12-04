b80c5ceba465e62ca8d52930f75e0ade745aa43e
mpashchenko <mpashchenko@ecisys.com>
2021-12-05 00:37:23 +0200

    0.3.0 - version fix

--- farmers-triple-items.js
+++ farmers-triple-items.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Farmers World Bot
 // @namespace    http://tampermonkey.net/
-// @version      0.2.3
+// @version      0.3.0
 // @description  Let's farm easy way
 // @author       ZRADNYK
 // @match        https://play.farmersworld.io
